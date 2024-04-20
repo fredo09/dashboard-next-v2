fe36e47256cb590617cc0f1307270a07dd2e2a7b
fredo09 <fred_15@outlokk.com>
2024-04-19 21:20:08 -0600

    refactor Navbar fos dashbaord

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,3 +3,4 @@
 */
 
 export { Sidebar } from './sidebar/Sidebar';
+export { Navbar } from './navbar/Navbar';
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+'use client';
 
-export const Navbar = () => {
+import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+//Interface Props component
+interface Props {
+  path: string,
+  icon: JSX.Element,
+  title: string,
+  subTitle: string
+}
+
+export const Navbar = ({ path, icon, title, subTitle }: Props) => {
+  console.log("🚀 ~ Navbar ~ path:", path);
+
+  const currentPath = usePathname();
+  console.log("🚀 ~ Navbar ~ currentPath:", currentPath);
+  
   return (
-    <div>Navbar</div>
+    <Link
+      href={path}
+      className={`
+        w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3  hover:bg-white/5 transition ease-linear duration-150
+        ${ currentPath === path ? 'bg-blue-800' : '' }
+      `}>
+      <div>
+        { icon }
+      </div>
+      <div className="flex flex-col">
+        <span className="text-lg font-bold leading-5 text-white">{ title }</span>
+        <span className="text-sm text-white/50 hidden md:block">{ subTitle }</span>
+      </div>
+    </Link>
   )
 }
