ff05457259333f4059a3c1ad412d4895319a4479
fredo09 <fred_15@outlokk.com>
2024-04-21 00:41:11 -0600

    add fetch to api pokemon

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-export default function PokemonPage ()  {
-  return (
-    <div>Pokemon 🚀 </div>
-  )
+
+const getApiPokemons = async ( limit = 20, offset = 0 ) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+    const data = await response.json();
+
+    return data;
+}
+
+export default async function PokemonPage() {
+    const pokemons = await getApiPokemons(151);
+    console.log("🚀 ~ PokemonPage ~ pokemons:", pokemons)
+
+    return (
+        <div>
+            Pokemon 🚀
+            {
+                JSON.stringify(pokemons)
+            }
+        </div>
+    )
 }
