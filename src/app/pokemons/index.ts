05c26ad810f3f07997ec9a1474fa46ead63468fc
fredo09 <fred_15@outlokk.com>
2024-04-21 00:42:09 -0600

    add type interfaces for api pokemon data

---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Export interfaces
+ */
+
+//* ussamos "type" en un export para exportar el tipo de dato que esa la interface 
+
+export type { PokemonsResponse } from './interfaces/pokemons-response';
+export type { SimplePokemon } from './interfaces/simple-pokemon';
