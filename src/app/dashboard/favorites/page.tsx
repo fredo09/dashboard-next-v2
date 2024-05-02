c69836012df3be092ce8a0ab051ed5d97d38cf28
fredo09 <fred_15@outlook.com>
2024-05-01 18:28:16 -0600

    add new page of favorites

---
+++ src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Metadata } from 'next';
+import { PokemonGrid } from '@/pokemons';
+
+
+export const metadata: Metadata = {
+    title: "Pokemons Favoritos",
+    description: "Ve tus pokemones favoritos",
+  };
+
+export default async function FavoritesPage() {
+    return (
+        <div className='flex flex-col'>
+            <span className='text-5xl my-2 '> Tus pokemons favoritos <small className='text-blue-400'>estatico</small></span>
+            
+            <PokemonGrid pokemons={[]} />
+        </div>
+    )
+}
